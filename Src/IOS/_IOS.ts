08374bff14c9e0f454c7deb97ae72500a32316d0
loadKK <1780319991@qq.com>
2023-07-11 00:06:06 +0800

    去除 logi

--- Src/IOS/_IOS.ts
+++ Src/IOS/_IOS.ts
@@ -6,7 +6,7 @@ export namespace IOS {
         try {
             let current_window = ObjC.classes.UIWindow.keyWindow();
             let view = current_window.recursiveDescription();
-            log.i(dump_ui.name, view);
+            log.d(dump_ui.name, view);
             return view;
         } catch (e) {
             return e;
