52034754f74417ec535084db92ddac5910539060
loadKK <notf@foxmail.com>
2023-09-03 17:10:05 +0800

    🚀优化代码格式以及Android hook 调用方式

--- Src/index.ts
+++ Src/index.ts
@@ -1,4 +1,4 @@
-import {FSLog} from "./FSLogger";
+import { FSLog } from "./FSLogger";
 import * as exports_Android from "./Android/__exports";
 import * as exports_IOS from "./IOS/__exports";
 
@@ -9,22 +9,21 @@ import * as exports_IOS from "./IOS/__exports";
 
 function android() {
     FSLog.bDebug = false  // 关闭 debug 级别的信息输出
-    FSLog.android()
-    const {Android} = exports_Android
+    // FSLog.android()
+    const { Android } = exports_Android
 
     // -------------------------------------------------------------------
 
     // hook
-    // Hook.jhook(`com.qq.lib.EncryptUtil`, `encrypt`,false) // 第三参数是否打印堆栈
-    // Hook.jhook(`inject.HttpUtils`, [`send`, `send2`], true) // 批量hook同一个类下的多个方法
-    // Hook.jhook(`com.sswl.sdk.h.o`, [`b`], true) // 批量hook同一个类下的多个方法
-
+    // Android.jhook(`com.qq.lib.EncryptUtil`, `encrypt`,false) // 第三参数是否打印堆栈
+    // Android.jhook(`inject.HttpUtils`, [`send`, `send2`], true) // 批量hook同一个类下的多个方法
+    Android.jhook(`com.sswl.sdk.h.o`, [`b`], true) // 批量hook同一个类下的多个方法
 }
 
 
 function ios() {
     // FSLog.ios()
-    const {IOS, Crypto, Network, Jailbreak, Env} = exports_IOS
+    const { IOS, Crypto, Network, Jailbreak, Env } = exports_IOS
 
     // -------------------------------------------------------------------
     // IOS.dump_ui()  // 查看界面UI
