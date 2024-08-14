7021586fa270aeb94be8f0355b2ae9737c3a12de
Root <notf@foxmail.com>
2024-08-14 17:07:40 +0800

    update bypass_frida_dbus.js

--- Src/Android/Bypass/bypass_frida_dbus.js
+++ Src/Android/Bypass/bypass_frida_dbus.js
@@ -1,5 +1,6 @@
 import {FSLog} from "../../FSLogger";
 
+// 等同于 ./frida-server -l 0.0.0.0:26151
 export const bypass_frida_dbus = () => {
     let connect = Module.findExportByName(null, "connect");
     if (!connect) {
@@ -10,8 +11,8 @@ export const bypass_frida_dbus = () => {
         onEnter: function (args) {
             let arg = args[1];
             let port = arg.add(0x2).readUShort();
-            if (port === 41577 /*|| port === 35421*/) {
-                arg.add(0x2).writeUShort(26151);  // 修改默认端口 27042(69A2大端) > 26151 | 27042 > 69A2 > 小端序（41577）
+            if (port === 41577 /*|| port === 35421*/) {  // A269 > 41577
+                arg.add(0x2).writeUShort(26151);  // 修改默认端口 27042(69A2大端) > 26151
                 FSLog.w("bypass_frida_dbus", `端口号${port}已被修改为26151`);
             }
         }
